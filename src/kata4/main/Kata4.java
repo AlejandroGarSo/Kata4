5c06a2b760b201e02f82b962421034ada17b553c
Usuario <Usuario@10.22.148.81>
2019-11-06 13:43:39 +0000

    Kata4V1

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -1,10 +1,26 @@
 
 package kata4.main;
 
+import java.util.List;
+import kata4.model.*;
+import kata4.view.*;
+
 public class Kata4 {
 
     public static void main(String[] args) {
+        String fileName = "email.txt";
+        List<Mail> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        
+        /*histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");*/
 
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
     }
     
 }
--- src/kata4/model/Mail.java
+++ src/kata4/model/Mail.java
@@ -2,4 +2,18 @@ package kata4.model;
 
 public class Mail {
 
+    private String mail;
+
+    public Mail(String mail) {
+        this.mail = mail;
+    }
+    
+    public String getDomain(){
+        return mail.substring(mail.indexOf("@")+1);
+    }
+    
+    public static boolean isMail(String line){
+        return line.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
+        
+    }
 }
