530cf8e3d2aca4e8bc339492aef3f4462f97c7fa
Usuario <Usuario@10.22.148.81>
2019-11-06 14:18:32 +0000

    Kata4 IteratorReader/Scanner

--- src/kata4/view/MailListReader.java
+++ src/kata4/view/MailListReader.java
@@ -1,6 +1,7 @@
 package kata4.view;
 
 import java.io.BufferedReader;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -16,17 +17,20 @@ public class MailListReader {
         List<Mail> list = new ArrayList<>();
     
         try{
-            BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
-            while(true){
-                String line = reader.readLine();
-                if(line == null)
-                    break;
+            Scanner reader = new Scanner(new BufferedReader(
+                    new FileReader(new File(fileName))));
+            while(reader.hasNext()){
+                String line = reader.next();
                 if(Mail.isMail(line))
                     list.add(new Mail(line));
             }
+            /*IteratorReader reader = new IteratorReader
+                (new BufferedReader(new FileReader(new File(fileName))));
+            for (String line : reader) {
+                list.add(new Mail(line));
+            }
+            */
         }catch(FileNotFoundException e){
-            System.out.println(e.getMessage());
-        }catch (IOException e){
             System.out.println(e.getMessage());
         }
         return list;
